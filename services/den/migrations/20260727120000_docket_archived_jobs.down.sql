30c90f094d7b2a37ec1f4d38e491945380fc614d
Hans Gerwitz <hans@gerwitz.com>
2026-07-27 13:53:03 +0000

    Allow focus selection job events in migrations

--- services/den/migrations/20260727120000_docket_archived_jobs.down.sql
+++ services/den/migrations/20260727120000_docket_archived_jobs.down.sql
@@ -3,7 +3,8 @@ ALTER TABLE bear_job_events ADD CONSTRAINT bear_job_events_event_type_check
     CHECK (event_type IN (
         'job_created', 'task_added', 'task_updated', 'criterion_evaluated',
         'job_blocked', 'job_completed', 'job_cancelled',
-        'handoff_requested', 'note_added', 'run_started', 'run_finished'
+        'handoff_requested', 'note_added', 'run_started', 'run_finished',
+        'focus_selected'
     ));
 
 ALTER TABLE bear_jobs DROP CONSTRAINT IF EXISTS bear_jobs_status_check;
--- services/den/migrations/20260727120000_docket_archived_jobs.up.sql
+++ services/den/migrations/20260727120000_docket_archived_jobs.up.sql
@@ -7,5 +7,6 @@ ALTER TABLE bear_job_events ADD CONSTRAINT bear_job_events_event_type_check
     CHECK (event_type IN (
         'job_created', 'task_added', 'task_updated', 'criterion_evaluated',
         'job_blocked', 'job_completed', 'job_cancelled', 'job_archived',
-        'handoff_requested', 'note_added', 'run_started', 'run_finished'
+        'handoff_requested', 'note_added', 'run_started', 'run_finished',
+        'focus_selected'
     ));
